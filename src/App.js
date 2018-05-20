2f635a9d9551fc1af91d4d0dc98607d02b72732a
Galloa118 <pewallin0527@gmail.com>
2018-05-20 01:33:53 -0700

    add validation of input field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
-import List from './List';
-import { Button } from 'reactstrap';
+import { Button, Input, Grid, Form, Col, Container, Row } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { InputGroup, InputGroupText, InputGroupAddon, Input, Grid, Form, Col, Container, Row } from 'reactstrap';
+
+import List from './List';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -13,38 +13,41 @@ class App extends Component {
       list_array: []
     };
   }
-  
   onChange = (event) => {
     this.setState({ list_term: event.target.value });
   }
 
   onSubmit = (event) => {
-    event.preventDefault();
-    this.setState({
-      list_term: '',
-      list_array: [...this.state.list_array, this.state.list_term]
-    });
+    // event.preventDefault();
+    if(this.state.list_term == '') {
+      alert('Input value is not empty!');
+    } else {
+      this.setState({
+        list_term: '',
+        list_array: [...this.state.list_array, this.state.list_term]
+      });
+    }
     // console.log(this.state.list_array);
   }
 
   render() {
     return (
-        <Container>
-            <div className="row input-button">
-                <h1><center>Todo List</center></h1>
-                <Col xl={8}>
-                    <input className="form-control float-left" value={this.state.list_term} onChange={this.onChange}  required />
-                </Col>
-                <Col xl={4}>
-                    <Button size="lg" className="add-button" color="success" onClick={this.onSubmit}>Add</Button>
-                </Col>
-            </div>
-            <div className="row">
-                <Col xl={8}>
-                    <List list_array={this.state.list_array} />
-                </Col>
-            </div>
-        </Container>
+      <Container>
+        <h1><center>Todo List</center></h1>
+        <div className="row input-button">
+          <Col xl={8}>
+            <Input className="form-control float-left" placeholder="Insert here..." value={this.state.list_term} onChange={this.onChange} />
+          </Col>
+          <Col xl={4}>
+            <Button size="lg" className="btn-block" color="success" onClick={this.onSubmit}>Add</Button>
+          </Col>
+        </div>
+        <div className="row">
+          <Col xl={8}>
+              <List list_array={this.state.list_array} />
+          </Col>
+        </div>
+      </Container>
     );
   }
 }
